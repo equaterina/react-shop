45a12b14cae672319db2eff2285d9bfb071cfac4
mermaidsknees <stravinskywellington@gmail.com>
2021-02-04 00:02:09 +0200

    Modified firebase config

---
+++ src/configs/firebase.js
@@ -0,0 +1,13 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBJqrwv46KKDXQvBp1WANXkQfNy8xAqmDo",
+    authDomain: "react-shop-2321e.firebaseapp.com",
+    projectId: "react-shop-2321e",
+    storageBucket: "react-shop-2321e.appspot.com",
+    messagingSenderId: "578391736226",
+    appId: "1:578391736226:web:034dcd30b6049fc2227cfb",
+    measurementId: "G-P7FH1VQJKB"
+};
+
+export default firebaseConfig;
